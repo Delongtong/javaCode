9d723c874eb8f4afd0e8ee273df00f4dee29cbba
Delongtong <tongdelong918@ymail.ne.jp>
2024-04-12 14:54:00 +0900

    6.4.12

---
+++ Boolean01.java
@@ -0,0 +1,13 @@
+public class Boolean01{
+	public static void main(String[] args) {
+//		
+		boolean isPass = false;
+		if(isPass == true) {
+			System.out.println("恭喜 通过");
+		}else {
+			System.out.println("很遗憾 未能通过");
+		}
+		
+		
+	}
+}
