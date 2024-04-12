fcfb38880bcd8b865da6c265655497a7a2a488a7
Delongtong <tongdelong918@ymail.ne.jp>
2024-04-12 14:39:58 +0900

    6.4.12

---
+++ Char02.java
@@ -0,0 +1,17 @@
+public class Char02{
+	public static void main(String[] args) {
+		char A = 'a';
+		int A1 = 123;
+		System.out.println(A);
+		char B = '佟';
+		System.out.println((double)B);
+		System.out.println((int)B);
+		char C = 20319;
+		System.out.println(C);
+		System.out.println((byte)B);
+//		char运算 b + 1之后 b转换 --> 98
+		char D = 'b' + 1;
+		System.out.println((int)D);
+		System.out.println(D);
+	}
+}
